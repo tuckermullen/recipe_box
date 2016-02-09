8bc8014dd2b879a1526401ac36e80b1123914919
Tucker Mullen <mullentucker@gmail.com>
2016-02-09 10:54:08 -0800

    In progress 2/9

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,6 @@ class Recipe < ActiveRecord::Base
 
   validates :title, :description, :image, presence: true
 
-  has_attached_file :image, styles: { medium: "400x400#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
